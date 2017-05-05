fe4dae3bddbf61992516f230e85b1bd80cbbfbb4
wjChoi000 <wjchoi222@gmail.com>
2017-05-05 16:42:44 +0900

    Add files via upload

---
+++ omogServer/userDAO.h
@@ -0,0 +1,31 @@
+#ifndef USERDAO_H
+#define USERDAO_H
+
+
+#include "/usr/include/mysql/mysql.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DB_HOST "127.0.0.1"
+#define DB_USER "root"
+#define DB_PASS "1234"
+#define DB_NAME "omoggame"
+#define DB_PORT 3306
+
+
+typedef struct userDTO{
+    char id[20];
+    char password[20];
+    char name[20];
+    int win;
+    int lose;
+    int draw;   
+}User;
+
+MYSQL *connection=NULL, conn;
+MYSQL_RES   *sql_result;
+MYSQL_ROW   sql_row;
+int query_stat;
+
+#endif
