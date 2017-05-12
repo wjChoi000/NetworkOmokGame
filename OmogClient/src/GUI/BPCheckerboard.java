af6922b4ba61c11485fb400e3be7374216cf385b
wjchoi <wjchoi222@gmail.com>
2017-05-13 02:14:13 +0900

    checkerboard GUI update

--- OmogClient/src/GUI/BPCheckerboard.java
+++ OmogClient/src/GUI/BPCheckerboard.java
@@ -1,6 +1,9 @@
 package GUI;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Vector;
 
 import javax.swing.*;
 
@@ -8,25 +11,52 @@ import GUI.Login.LoginActionListener;
 import GUI.Login.SignupActionListener;
 
 public class BPCheckerboard extends JPanel {
-
-	private int w = 1100;
-	private int h = 1100;
 	
 	private int m =100;
+	private int bgeb = 70;
+	private int w = m*2+bgeb*18;
+	private int h = w;
+	int ovalSize = 14;
+	
+	private String currentPath = this.getClass().getResource("").getPath();
+	private String blackStonePath="image/blackStone.png";
+	private String whiteStonePath="image/WhiteStone.png";
+	private ImageIcon blackStoneIcon = new ImageIcon(currentPath+blackStonePath);
+	private ImageIcon whiteStoneIcon = new ImageIcon(currentPath+whiteStonePath);
+	private Image blackStoneImage = blackStoneIcon.getImage();
+	private Image whiteStoneImage = whiteStoneIcon.getImage();
+	private Vector<Point> whitePV = new Vector<Point>();
+	private Vector<Point> blackPV = new Vector<Point>();
+	
+	private Point p;
+	
 	BPCheckerboard(){
-		
 		init();
 		
 	}
+	
+	int temp = 0;
+	
 	private void init(){
 		this.setLayout(null);
+		this.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e){
+				Point p =putStonePoint(e.getPoint());
+				if(p != null){
+					if(temp%2 ==0)
+						whitePV.add(p);
+					else
+						blackPV.add(p);
+					temp++;
+					repaint();
+				}
+			}
+		});
 		
-		
-
 		this.setSize(w,h);
 	}
 
-	
+
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		//set background
@@ -35,15 +65,52 @@ public class BPCheckerboard extends JPanel {
 	
 		//draw checker 
 		Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(3));
+        g2.setStroke(new BasicStroke(5));
 		g2.setColor(Color.black);
 		
-		int bgeb = 50;
-		//18*50 = 900
+		g2.drawLine(m, m+0*bgeb, m+bgeb*18, m+0*bgeb);
+		g2.drawLine(m, m+18*bgeb, m+bgeb*18, m+18*bgeb);
+		g2.drawLine(m+0*bgeb,m,m+0*bgeb,m+bgeb*18);
+		g2.drawLine(m+18*bgeb,m,m+18*bgeb,m+bgeb*18);
+
+        g2.setStroke(new BasicStroke(3));
+		
 		for(int i =0; i<19; i++){
 			g2.drawLine(m, m+i*bgeb, m+bgeb*18, m+i*bgeb);
 			g2.drawLine(m+i*bgeb,m,m+i*bgeb,m+bgeb*18);
 		}
+		
+		g.setColor(Color.black);
+
+		for(int i =0; i<3; i++)
+			for(int j =0; j <3; j++)
+				g.fillOval(m+bgeb*(3+i*6)-ovalSize/2,m+bgeb*(3+j*6)-ovalSize/2,ovalSize,ovalSize);
+
+		for(int i=0; i < whitePV.size();i++){
+			Point p = whitePV.elementAt(i);
+			g.drawImage(whiteStoneImage,(int)p.getX() ,(int)p.getY() , bgeb,bgeb,this);
+		}
+		for(int i =0; i< blackPV.size(); i++){
+			Point p = blackPV.elementAt(i);
+			g.drawImage(blackStoneImage, (int)p.getX(), (int)p.getY(), bgeb,bgeb,this);
+		}
+	}
+	
+	private Point putStonePoint(Point p){
+		int start = m-bgeb/2;
+		int x = (int)p.getX();
+		int y =(int)p.getY();
+		
+		if(x >=start && x<=start+bgeb*19 && y>=start && y<=start+bgeb*19){
+			x = (x-start)/bgeb;
+			y = (y-start)/bgeb;
+			Point result;	
+			result = new Point(start+bgeb*x,start+bgeb*y);
+			System.out.println(x+", " +y);
+			System.out.println(result.toString());
+			return result;
+		}else
+			return null;
 		
 	}
 	
---
+++ OmogClient/src/Soket/Test.java
@@ -0,0 +1,21 @@
+package Soket;
+
+import java.net.ServerSocket;
+
+public class Test {
+
+	
+	private String ipAddress;
+	private int port;
+	
+	
+
+	/*public static void main(String[] args){
+		
+		ServerSocket serverSocket = null;
+		try{
+			
+		}
+		
+	}*/
+}
