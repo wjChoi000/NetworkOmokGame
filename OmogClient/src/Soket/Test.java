cb95dafb6bd127f5e7d535daef1c84b0999d361c
whchoi222@gmail.com <whchoi@whchoi>
2017-05-20 16:58:44 +0900

    11

--- OmogClient/src/Soket/Test.java
+++ OmogClient/src/Soket/Test.java
@@ -1,21 +1,46 @@
 package Soket;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Test {
 
 	
-	private String ipAddress;
-	private int port;
+	private static final String ipAddress ="127.0.0.1";
+	private static final int port = 8090;
+	private static final int BUFFERSIZE= 1024;
 	
 	
 
-	/*public static void main(String[] args){
+	public static void main(String[] args){
+		Socket socket = null;
+		BufferedOutputStream bos = null;
+		BufferedInputStream bis = null;
 		
-		ServerSocket serverSocket = null;
 		try{
+			socket = new Socket(ipAddress,port);
+			bos = new BufferedOutputStream(socket.getOutputStream());
+			byte[] c = new byte[BUFFERSIZE];
+			c = "abc".getBytes();
+			bos.write(c);
+			bos.flush();
 			
+			bis = new BufferedInputStream(socket.getInputStream());
+			byte[] s = new byte[BUFFERSIZE];
+			System.out.println(s.toString());
+			
+			bos.close();
+			bis.close();
+		}catch(ConnectException ce){
+			ce.printStackTrace();
+		}catch(IOException ie){
+			ie.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-		
-	}*/
+	}
 }
